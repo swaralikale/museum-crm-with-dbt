a264d8c149f9c0b4ef33da3b77d0e2ab55463d5c
swaralikale <k.swarali@op.iitg.ac.in>
2026-08-24 20:12:47 +0530

    Add files via upload

--- dbt_project/data-tests/assert_no_duplicate_contacts.sql
+++ dbt_project/data-tests/assert_no_duplicate_contacts.sql
@@ -1,1 +1,16 @@
+-- Singular data test: Detect potential duplicate contacts
+with potential_duplicates as (
+    select
+        first_name,
+        last_name,
+        phone,
+        count(distinct contact_id) as contact_count
+    from {{ ref('stg_contacts') }}
+    where phone is not null
+        and first_name is not null
+        and last_name is not null
+    group by first_name, last_name, phone
+    having count(distinct contact_id) > 1
+)
 
+select * from potential_duplicates
