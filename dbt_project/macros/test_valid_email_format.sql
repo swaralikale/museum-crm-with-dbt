154a6216fa5d26adf3f68511e2ceca738352cf93
swaralikale <k.swarali@op.iitg.ac.in>
2026-08-24 20:04:01 +0530

    Add files via upload

--- dbt_project/macros/test_valid_email_format.sql
+++ dbt_project/macros/test_valid_email_format.sql
@@ -1,1 +1,14 @@
+{% test valid_email_format(model, column_name) %}
 
+with validation as (
+    select
+        {{ column_name }} as email_value
+    from {{ model }}
+    where {{ column_name }} is not null
+)
+
+select email_value
+from validation
+where email_value not like '%_@_%.__%'
+
+{% endtest %}
